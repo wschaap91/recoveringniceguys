353dd84cdd176b6189769686b7214c28ba42b69c
Wouter Schaap <hallo@koffiemetschaap.nl>
2023-11-16 15:21:27 +0100

    Fix routing issue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <script setup lang="ts">
-  import { watch } from 'vue';
+  import { onMounted, watch } from 'vue';
   import { useGeneralStore } from './stores/generalStore';
   import { storeToRefs } from 'pinia';
   import router from './router';
   const store = useGeneralStore();
   const { token } = storeToRefs(store);
 
-  watch(token, () => {
-    if (token.value && token.value !== 'undefined') return;
-    router.push({ name: 'login' });
+  onMounted(() => {
+    watch(token, () => {
+      if (token.value && token.value !== 'undefined') return;
+      router.push({ name: 'login' });
+    }, { immediate: true })
   })
+
 </script>
 
 <template>
